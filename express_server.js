8526c4d833cb4ffaae0e6bf1cd080e6ad95067ce
dwcalade <dwcalade@yahoo.ca>
2022-04-20 21:36:02 +0000

    Implement Logout Client & Server logic CHANGES1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
+
+const bodyParser = require("body-parser");
+const users = {};
 
 app.set("view engine", "ejs");
 
@@ -9,16 +13,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
@@ -28,36 +30,100 @@ app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+// URL index page
+app.get('/urls', (req, res) => {
+  let templateVars = { urls: urlDatabase, user: users[req.cookies['user_id']] };
+  res.render('urls_index', templateVars);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+//generates a shortURL then database and redirects /urls/shortURL
+app.post('/urls', (req, res) => {
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
+})
+
+// new url creation page
+app.get('/urls/new', (req, res) => {
+  let templateVars = {user: users[req.cookies['user_id']]};
+  res.render('urls_new', templateVars);
 });
 
+//short URL that shows both versions
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+// updates the longURL in the database
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = req.body.updatedURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+// deletes a url from database, redirects to index page
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls');
 });
 
-app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  if (longURL) {
+    res.redirect(urlDatabase[req.params.shortURL]);
+  } else {
+    res.statusCode = 404;
+    res.send('<h2>404 Not Found<br>This short URL does not exist.</h2>')
+  }
 });
 
-function generateRandomString() {
+// The Login page
+app.get('/login', (req, res) => {
+  let templateVars = {user: users[req.cookies['user_id']]};
+  res.render('urls_login', templateVars);
+});
 
-}
+// Login 
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+// Logout 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+})
+
+
+// registration page
+app.get('/register', (req, res) => {
+  let templateVars = {user: users[req.cookies['user_id']]};
+  res.render('urls_registration', templateVars);
+});
+// register functionality
+app.post('/register', (req, res) => {
+  if (req.body.email && req.body.password) {
+    if (!findEmailInDatabase(req.body.email)) {
+      const userID = generateRandomString();
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+
+app.get('/', function (req, res) {
+  // Cookies that have not been signed
+  console.log('Cookies: ', req.cookies)
+
+  // Cookies that have been signed
+  console.log('Signed Cookies: ', req.signedCookies)
+})
